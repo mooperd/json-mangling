1ff7caa7ee11f03975738fedbee0e335423c5567
Andrew Holway <a.holway@dcmn.com>
2016-09-17 14:12:10 +0200

    nearly there

--- mangle.py
+++ mangle.py
@@ -18,31 +18,47 @@ def load_networks(networks_dict):
             network = Network(network_dict["name"])
             network.cidr = network_dict["cidr"]
             network.subnets = list()
-            pprint.pprint("####")
-            pprint.pprint(network)
             if "subnets" in network_dict:
                 for subnet_dict_1 in network_dict["subnets"]:
                     subnet_1 = Network(subnet_dict_1["name"])
                     subnet_1.cidr = subnet_dict_1["cidr"]
                     subnet_1.subnets = list()
-                    pprint.pprint("#### ####")
-                    pprint.pprint(subnet_1)
+
                     if "subnets" in subnet_dict_1:
                         for subnet_dict_2 in subnet_dict_1["subnets"]:
-                            pprint.pprint(subnet_dict_2)
                             subnet_2 = Network(subnet_dict_2["name"])
                             subnet_2.cidr = subnet_dict_2["cidr"]
                             subnet_2.subnets = list()
-                            pprint.pprint("#### #### ####")
-                            pprint.pprint(subnet_2)
-                            subnets.subnets.append(subnet_2)
-                    # network_list.subnets.append(subnet_1)
-                    # pprint.pprint(subnet)
+
+                            #network.subnets.subnets.append(subnet_2)
+
+                            #######################
+                            #pprint.pprint("2############")
+                            #pprint.pprint(subnet_2)
+                            #######################
+
+                    network.subnets.append(subnet_1)
+
+                    #######################
+                    #pprint.pprint("1######")
+                    #pprint.pprint(subnet_1)
+                    #######################
+
             network_list.append(network)
+
+            pprint.pprint(network_list)
+            #######################
+            #pprint.pprint("0###")
+            #pprint.pprint(network)
+            #######################
+
         return network_list
 
 if __name__ == '__main__':
     json_file = open_file()
     foo = load_networks(json_file)
-    # pprint.pprint(foo)
+    pprint.pprint("##########################")
+    pprint.pprint("#######  RESULT  #########")
+    pprint.pprint("##########################")
+    pprint.pprint(foo)
     # output = network.traverse()
