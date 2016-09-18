3dea6a2426dc3764988e4617628263079d9b5a3c
Andrew Holway <a.holway@dcmn.com>
2016-09-18 23:23:43 +0200

    fixed

--- mangle.py
+++ mangle.py
@@ -18,47 +18,34 @@ def load_networks(networks_dict):
             network = Network(network_dict["name"])
             network.cidr = network_dict["cidr"]
             network.subnets = list()
+
             if "subnets" in network_dict:
+                subnet_1_list = []
                 for subnet_dict_1 in network_dict["subnets"]:
                     subnet_1 = Network(subnet_dict_1["name"])
                     subnet_1.cidr = subnet_dict_1["cidr"]
                     subnet_1.subnets = list()
 
                     if "subnets" in subnet_dict_1:
+                        subnet_2_list = []
                         for subnet_dict_2 in subnet_dict_1["subnets"]:
                             subnet_2 = Network(subnet_dict_2["name"])
                             subnet_2.cidr = subnet_dict_2["cidr"]
                             subnet_2.subnets = list()
 
-                            #network.subnets.subnets.append(subnet_2)
-
-                            #######################
-                            #pprint.pprint("2############")
-                            #pprint.pprint(subnet_2)
-                            #######################
+                            subnet_1.subnets.append(subnet_2)
 
                     network.subnets.append(subnet_1)
 
-                    #######################
-                    #pprint.pprint("1######")
-                    #pprint.pprint(subnet_1)
-                    #######################
-
             network_list.append(network)
-
-            pprint.pprint(network_list)
-            #######################
-            #pprint.pprint("0###")
-            #pprint.pprint(network)
-            #######################
-
+        pprint.pprint(network_list)
         return network_list
 
 if __name__ == '__main__':
     json_file = open_file()
     foo = load_networks(json_file)
-    pprint.pprint("##########################")
-    pprint.pprint("#######  RESULT  #########")
-    pprint.pprint("##########################")
+    #pprint.pprint("##########################")
+    #pprint.pprint("#######  RESULT  #########")
+    #pprint.pprint("##########################")
     pprint.pprint(foo)
-    # output = network.traverse()
+    #output = network.traverse()
--- model.py
+++ model.py
@@ -11,22 +11,3 @@ class Network(object):
             self.cidr,
             self.subnets,
         )
-    """
-    def load_it(self, networks_dict):
-        self.name = networks_dict["name"]
-        self.cidr = networks_dict["cidr"]
-        try:
-            for network in networks_dict["subnet"]:
-                if "subnets" in network:
-                    self.traverse(network)
-        except TypeError:
-            pass
-
-    def traverse(self, networks_dict):
-        #pprint.pprint(networks_dict)
-        network = Network()
-        self.name = networks_dict["name"]
-        self.cidr = networks_dict["cidr"]
-        self.subnet = network.traverse(networks_dict)
-        pprint.pprint(self)
-    """
